2f9e8e226252b363a79b147b7113cfc840e16e7f
ssfdust <ssfdust@gmail.com>
2022-03-08 10:23:29 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -16,46 +16,48 @@ macro_rules! nth {
     };
 }
 
+fn is_not_cdrom(disk_name: &str) -> bool {
+    !(disk_name.contains("hdc") || disk_name.contains("sr") || disk_name.contains("scd"))
+}
+
+fn is_physical_device(disk_name: &str) -> bool {
+    let device_path = Path::new(&format!(
+        "/sys/block/{}/device",
+        disk_name.replace("/", "!")
+    ));
+    device_path.exists() && is_not_cdrom(disk_name)
+}
+
 fn list_disks() -> io::Result<Vec<String>> {
     File::open("/proc/diskstats").and_then(|file_in| {
         let file_reader = BufReader::with_capacity(2048, file_in);
         Ok(file_reader
             .lines()
-            .filter_map(|line_in| match line_in {
-                Ok(line) => {
-                    let mut fields = line.split_whitespace();
-                    match nth!(fields, 2).and_then(|name| match name {
-                        name if name.contains("sr") => Ok(None),
-                        name if name.contains("scd") => Ok(None),
-                        name if name.contains("hdc") => Ok(None),
-                        name if Path::new(&format!(
-                            "/sys/block/{}/device",
-                            name.replace("/", "!")
-                        ))
-                        .exists() =>
-                        {
-                            Ok(Some(name.to_owned()))
-                        }
-                        _ => Ok(None),
-                    }) {
-                        Ok(Some(val)) => Some(val),
-                        _ => None,
-                    }
-                }
-                _ => None,
+            .filter_map(|line_in| {
+                line_in
+                    .and_then(|line| {
+                        let mut fields = line.split_whitespace();
+                        nth!(fields, 2).and_then(|name| {
+                            Ok({
+                                if is_physical_device(name) {
+                                    Some(name.to_owned())
+                                } else {
+                                    None
+                                }
+                            })
+                        })
+                    })
+                    .unwrap_or(None)
             })
             .collect::<Vec<String>>())
     })
 }
 
-fn read_to_i64(filename: &str) -> Result<i64, String> {
-    match read_to_string(filename).and_then(|content| Ok(content.trim().parse::<i64>())) {
-        Ok(Ok(intdata)) => Ok(intdata),
-        _ => Err("Failed".to_owned()),
-    }
+fn read_to_i64(filename: &str) -> io::Result<i64> {
+    read_to_string(filename).and_then(|content| Ok(content.trim().parse::<i64>().unwrap_or(0)))
 }
 
-fn get_disk_size(disk_name: &str) -> Result<i64, String> {
+fn get_disk_size(disk_name: &str) -> io::Result<i64> {
     let sector_unit_file = format!("/sys/block/{}/queue/logical_block_size", disk_name);
     let sector_size_file = format!("/sys/block/{}/size", disk_name);
     read_to_i64(&sector_size_file).and_then(|sector_size| {
@@ -63,17 +65,18 @@ fn get_disk_size(disk_name: &str) -> Result<i64, String> {
     })
 }
 
-fn main() {
-    match list_disks().and_then(|disks| {
+fn get_disk_total_size() -> io::Result<i64> {
+    list_disks().and_then(|disks| {
         Ok(disks
             .into_iter()
-            .map(|x| match get_disk_size(&x) {
-                Ok(val) => val,
-                Err(_) => 0,
-            })
+            .map(|x| get_disk_size(&x).unwrap_or(0))
             .fold(0, |acc, x| acc + x))
-    }) {
-        Ok(total_size) => println!("{:.1}", total_size as f64 / 1024.0 / 1024.0 / 1024.0),
-        _ => println!("Failed to get disk size."),
+    })
+}
+
+fn main() {
+    match get_disk_total_size() {
+        Ok(total_size) => println!("{}", total_size),
+        _ => println!("Failed to get disk size.")
     }
 }
